fddff619882dbeea0d76e6e65c70da6074251384
ximena-bogado <63512143+ximena-bogado@users.noreply.github.com>
2023-04-20 21:02:10 -0300

    bonus esconderse tras una pared

--- entrega-5-ximena-bogado/entities/enemies/Turret.gd
+++ entrega-5-ximena-bogado/entities/enemies/Turret.gd
@@ -7,7 +7,6 @@ export (PackedScene) var projectile_scene
 
 var player
 var projectile_container
-
 var target:Node2D
 
 func _ready():
@@ -18,7 +17,17 @@ func initialize(container, turret_pos, projectile_container):
 	global_position = turret_pos
 	self.projectile_container = projectile_container
 	
-
+func _physics_process(delta):
+	if target != null:
+		var space_state = get_world_2d().direct_space_state
+		var result = space_state.intersect_ray(global_position, target.global_position,
+							[self, target], collision_mask)
+		if !result.empty() && !fire_timer.is_stopped():
+			fire_timer.stop()
+		elif result.empty() && fire_timer.is_stopped():
+			fire_timer.start()
+		
+							
 func fire_at_player():
 	var proj_instance = projectile_scene.instance()
 	proj_instance.initialize(projectile_container, fire_position.global_position, fire_position.global_position.direction_to(target.global_position))
